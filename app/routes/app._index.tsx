5fe7d4d97234b254933ed42da5b31c4656209392
szabolts <hogyeszabit@gmail.com>
2024-10-23 17:28:42 +0200

    test: pipeline trigger test commit

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -125,7 +125,7 @@ export default function Index() {
               <BlockStack gap="500">
                 <BlockStack gap="200">
                   <Text as="h2" variant="headingMd">
-                    Congrats on creating a new Shopify app 🎉
+                    Heloszia
                   </Text>
                   <Text variant="bodyMd" as="p">
                     This embedded app template uses{" "}
